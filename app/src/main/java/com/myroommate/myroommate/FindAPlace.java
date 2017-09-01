637f4e78dbe6a670ed6ff3e87409edcc6c1a01da
ganesh3s3 <miffychitu@gmail.com>
2017-09-01 12:08:51 +0530

    implemented the go up button on find a place activity

--- app/src/main/java/com/myroommate/myroommate/FindAPlace.java
+++ app/src/main/java/com/myroommate/myroommate/FindAPlace.java
@@ -2,6 +2,7 @@ package com.myroommate.myroommate;
 
 import android.content.res.Resources;
 import android.content.res.TypedArray;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
@@ -10,6 +11,7 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.AdapterView;
+import android.widget.Toolbar;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -19,8 +21,10 @@ public class FindAPlace extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_find_a_place);
+
+        ActionBar ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
 
         final List<String> list0 = new ArrayList<String>();
         list0.add("Select One");
@@ -182,19 +186,7 @@ public class FindAPlace extends AppCompatActivity {
 
             public void onNothingSelected(AdapterView<?> parentView)
             {
-
             }
         });
-
-
-
-
-
-
-
     }
-
-
-
 }
-
