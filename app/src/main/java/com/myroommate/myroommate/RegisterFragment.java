b9ca1808ff5baaa38ff675e26f40adfd5c9e49a5
ganesh3s3 <miffychitu@gmail.com>
2017-09-07 12:17:58 +0530

    Implemented RegisterFragment with a working online database attached

--- app/src/main/java/com/myroommate/myroommate/RegisterFragment.java
+++ app/src/main/java/com/myroommate/myroommate/RegisterFragment.java
@@ -6,8 +6,27 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+import java.util.HashMap;
 
 public class RegisterFragment extends Fragment {
+
+    Button register;
+    EditText First_Name, Last_Name, Email, Password ;
+    String F_Name_Holder, L_Name_Holder, EmailHolder, PasswordHolder;
+    String finalResult ;
+    String HttpURL = "https://myroommate.000webhostapp.com/UserRegistration.php";
+    Boolean CheckEditText ;
+    ProgressDialog progressDialog;
+    HashMap<String,String> hashMap = new HashMap<>();
+    HttpParse httpParse = new HttpParse();
 
     public static final String TITLE = "Register";
 
@@ -16,9 +35,111 @@ public class RegisterFragment extends Fragment {
         return new RegisterFragment();
     }
 
-    @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.register_fragment, container, false);
+
+        View RootView = inflater.inflate(R.layout.register_fragment, container, false);
+
+        //Assign Id'S
+        First_Name = (EditText)RootView.findViewById(R.id.register_firstname);
+        Last_Name = (EditText)RootView.findViewById(R.id.register_lastname);
+        Email = (EditText)RootView.findViewById(R.id.register_email);
+        Password = (EditText)RootView.findViewById(R.id.register_password);
+        register = (Button)RootView.findViewById(R.id.email_register_button);
+
+        //Adding Click Listener on button.
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                // Checking whether EditText is Empty or Not
+                CheckEditTextIsEmptyOrNot();
+
+                if(CheckEditText){
+
+                    // If EditText is not empty and CheckEditText = True then this block will execute.
+
+                    UserRegisterFunction(F_Name_Holder,L_Name_Holder, EmailHolder, PasswordHolder);
+
+                }
+                else {
+
+                    // If EditText is empty then this block will execute .
+                    Toast.makeText(getActivity(), "Please fill all the form fields.", Toast.LENGTH_LONG).show();
+
+                }
+
+
+            }
+        });
+
+        return RootView;
     }
+
+    public void CheckEditTextIsEmptyOrNot(){
+
+        F_Name_Holder = First_Name.getText().toString();
+        L_Name_Holder = Last_Name.getText().toString();
+        EmailHolder = Email.getText().toString();
+        PasswordHolder = Password.getText().toString();
+
+
+        if(TextUtils.isEmpty(F_Name_Holder) || TextUtils.isEmpty(L_Name_Holder) || TextUtils.isEmpty(EmailHolder) || TextUtils.isEmpty(PasswordHolder))
+        {
+
+            CheckEditText = false;
+
+        }
+        else {
+
+            CheckEditText = true ;
+        }
+
+    }
+
+    public void UserRegisterFunction(final String F_Name, final String L_Name, final String email, final String password){
+
+        class UserRegisterFunctionClass extends AsyncTask<String,Void,String> {
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+
+                progressDialog = ProgressDialog.show(getActivity(),"Loading Data",null,true,true);
+            }
+
+            @Override
+            protected void onPostExecute(String httpResponseMsg) {
+
+                super.onPostExecute(httpResponseMsg);
+
+                progressDialog.dismiss();
+
+                Toast.makeText(getActivity(),httpResponseMsg, Toast.LENGTH_LONG).show();
+
+            }
+
+            @Override
+            protected String doInBackground(String... params) {
+
+                hashMap.put("f_name",params[0]);
+
+                hashMap.put("L_name",params[1]);
+
+                hashMap.put("email",params[2]);
+
+                hashMap.put("password",params[3]);
+
+                finalResult = httpParse.postRequest(hashMap, HttpURL);
+
+                return finalResult;
+            }
+        }
+
+        UserRegisterFunctionClass userRegisterFunctionClass = new UserRegisterFunctionClass();
+
+        userRegisterFunctionClass.execute(F_Name,L_Name,email,password);
+    }
+
+
 }
